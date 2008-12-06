d39cbaed5dd3350fa4f4d55462c181307650cafe
Iustin Pop <iusty@k1024.org>
2008-12-06 14:50:31 +0900

    Add copyright information
    For some reason this was missing from the source file.

--- bakonf.py
+++ bakonf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 #
+# Copyright (C) 2002, 2004, 2008 Iustin Pop
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
