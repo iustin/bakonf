d674c2e3f769ac24aee0c101113704fa9435cb62
Iustin Pop <iustin@k1024.org>
2019-11-24 18:27:53 +0100

    Add initial setup.py and MANIFEST file
    Not complete yet…

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+  name="bakonf",
+  version="0.7.0",
+  scripts=["bakonf.py"],
+  python_requires=">=3.5",
+  install_requires="bsddb3",
+
+  author="Iustin Pop",
+  author_email="iustin@k1024.org",
+  description="Simple backup tool",
+  keywords="backup",
+  url="http://github.com/iustin/bakonf",
+  project_urls={
+    "Bug Tracker": "https://github.com/iustin/bakonf/issues",
+    },
+  classifiers=[
+    "Environment :: Console",
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+    "Operating System :: Unix",
+    "Operating System :: POSIX :: Linux",
+    "Topic :: System :: Archiving :: Backup",
+    "Topic :: Utilities",
+  ],
+)
